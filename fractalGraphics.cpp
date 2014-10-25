6d7025f3b33e6f549f3d1f7624016bf6075fab7b
aspirationunknown <aspirationunknown@gmail.com>
2014-10-24 20:06:44 -0600

    made changes

--- fractalGraphics.cpp
+++ fractalGraphics.cpp
@@ -10,19 +10,62 @@ void drawLine(Point first, Point second, const float color[])
     glFlush();
 }
 
-void drawPolygon(Point poly[], int size, const float color[])
+void drawPolygon(Polygon poly[], int size, const float color[])
 {
     int i;
 
-    for(i = 0; i < size; ++i)
+    for(i = 0; i < poly.length; ++i)
     {
         //draw a line between each pair of points
-        drawLine(poly[i], poly[(i+1)%size], color);
+        drawLine(poly[i], poly[(i+1)%poly.length], color);
     }
 
 }
 
+void drawField(int screen_height, int screen_width)
+{
+    Point poly[4];
+
+    poly[0].x = 5;
+    poly[0].y = screen_height;
+
+    poly[1].x = -5;
+    poly[1].y = screen_height;
+
+    poly[2].x = -5;
+    poly[2].y = -screen_height;
+
+    poly[3].x = 5;
+    poly[3].y = -screen_height;
+
+    drawLine(poly[0], poly[1]);
+    drawLine(poly[1], poly[2]);
+    drawLine(poly[2], poly[3]);
+    drawLine(poly[3], poly[0]);
+    
+}
+
 //function to replace a line with the generator pattern
+
+
+ /***************************************************************************//**
+ * DrawBitmapString
+ * Authors - Dr. John Weiss
+ *
+ * Draws a stroke string for a game menu
+ * Parameters -
+    string - the string to draw
+    x - the x location to draw the string on the screen
+    y - the y location to draw the string on the screen
+    color -the color the draw the string, stored in a 3-element array
+ ******************************************************************************/
+void DrawBitmapString( const char *string, float x, float y, float color[] )
+{
+    glColor3f( color[0], color[1], color[2] );
+    glRasterPos2f( x, y );
+    // while ( *string ) glutBitmapCharacter( GLUT_BITMAP_TIMES_ROMAN_24, *string++ );
+    glutBitmapString( GLUT_BITMAP_HELVETICA_18, (const unsigned char *)string );
+}
 
 
 
--- structs.h
+++ structs.h
@@ -6,23 +6,24 @@ struct Point
     int y;
 };
 
-<<<<<<< HEAD
-//functions
-void rotate(Points poly[]);
-void rotatePoints(/*add params*/);
-void scale(Points poly[]);
-void scalePoints(/*add params*/);
-void translate(Points poly[]);
-void translatePoints(/*add params*/);
-
-=======
 struct Polygon
 {
     Point* points;
     int length = 0;   // the number of points  
->>>>>>> cc593631123f2a5cf5da7eeb5ad0347c0b2bcae2
 
     void close();
     void addPoint( Point point );
     void addPoint( int x, int y );
 };
+
+
+//functions
+void rotate(Polygon poly[]);
+void rotatePoints(/*add params*/);
+void scale(Polygon  poly[]);
+void scalePoints(/*add params*/);
+void translate(Polygon  poly[]);
+void translatePoints(/*add params*/);
+
+
+
